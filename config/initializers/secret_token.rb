73170a209ac0475745526a48052b7966c63cfe01
Dev Server <dev@dev.com>
2011-06-22 17:12:25 +0000

    Testing git out

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstest::Application.config.secret_token = '5f76f0435d24c4bf60bb75a54e2be350d76a2a4a5f8c294af30e567ee1f0ab77ba9feaa9715ef775696fa60eb74575cad2cd487d47f31c4306864af53b5bc1f0'
